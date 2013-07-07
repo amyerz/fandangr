8a20b9df38ccea4470868657e7f4ddf2ed8ff012
Amy Erz <wingmerz@gmail.com>
2013-07-07 13:44:56 -0700

    create fandagr app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fandangr::Application.config.secret_token = 'b12cd532c021783f19ffd45e104190c674e79c8376b68e2d9692eae72dac54a090ee1f81e8df95ffefd4fac1996855210a42ef79551da794444b83aae823ad05'
