cf8c6af99e5050c30216f322a549016108f54f4e
Amy Erz <wingmerz@gmail.com>
2013-07-07 14:54:01 -0700

    update assoications

--- app/models/showing.rb
+++ app/models/showing.rb
@@ -1,3 +1,5 @@
 class Showing < ActiveRecord::Base
+  belongs_to: movie
+  has_many: tickets
   attr_accessible :datetime, :movie_id
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
+	has_many: tickets
   attr_accessible :email, :password_digest, :username
 end
