b28834bdbe35a4f307ac6abec496d8a7292c00df
Amy Erz <wingmerz@gmail.com>
2013-07-07 17:56:35 -0700

    add link to user sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
 	has_many :tickets
-  attr_accessible :email, :password_digest, :username
+	has_secure_password
+  attr_accessible :email, :password_digest, :username , :password_confirmation
+  validates_uniqueness_of :email
 end
