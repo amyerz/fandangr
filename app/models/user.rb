1714985cf47a3155c98c6f3f7aa4794eb42e72ee
Amy Erz <wingmerz@gmail.com>
2013-07-07 19:11:16 -0700

    fixed sessions route

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ActiveRecord::Base
 	has_many :tickets
 	has_secure_password
   attr_accessible :email, :password, :username , :password_confirmation
-  validates_uniqueness_of :email
+  validates_uniqueness_of :email, :username
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Fandangr::Application.routes.draw do
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
  
+  resources :sessions
   resources :showings
 
 
